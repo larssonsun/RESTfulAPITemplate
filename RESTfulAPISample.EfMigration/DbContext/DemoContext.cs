17e73794f11eb19106baaa08ad87c3d8b33c3753
磊 孙 <77540975@qq.com>
2020-03-17 09:37:33 +0800

    Separate ef migrations into separate projects.

---
+++ RESTfulAPISample.EfMigration/DbContext/DemoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RESTfulAPISample.Core.Entity;
+using RESTfulAPISample.EfMigration.Demo.EntityConfiguration;
+
+namespace RESTfulAPISample.EfMigration.Demo
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
