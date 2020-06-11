3077ad8a37cf6dd85a7cad907ef003a5e6fd08cd
磊 孙 <77540975@qq.com>
2020-06-11 10:53:54 +0800

    Removed dependency on Id segmentation of target object from paging function.

--- templates/RESTfulAPITemplate.Core/Entities/Entity.cs
+++ templates/RESTfulAPITemplate.Core/Entities/Entity.cs
@@ -1,19 +1,6 @@
-﻿#if (RESTFULAPIHELPER)
-
-using Larsson.RESTfulAPIHelper.Interface;
-
-#endif
-
-namespace RESTfulAPITemplate.Core.Entity
+﻿namespace RESTfulAPITemplate.Core.Entity
 {
     public class Entity 
-    
-#if (RESTFULAPIHELPER)
-
-    : IEntity
-
-#endif
-
     {
         public System.Guid Id { get; set; }
     }
