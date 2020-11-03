a26eb6bbefbeccb07b36159483f971f25eef3ca9
磊 孙 <77540975@qq.com>
2020-11-03 10:18:38 +0800

    Pumped ef version to 3.1.9.

--- templates/RESTfulAPITemplate.EfMigration/Migrations/20200317045235_1st_Migration.Designer.cs
+++ templates/RESTfulAPITemplate.EfMigration/Migrations/20200317045235_1st_Migration.Designer.cs
@@ -17,7 +17,7 @@ namespace RESTfulAPITemplate.EfMigration.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.2")
+                .HasAnnotation("ProductVersion", "3.1.9")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
